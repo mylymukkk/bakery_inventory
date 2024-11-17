232cbde954a700be1c388aa5014b67fd45ffab56
Mylymuk Oksana <oksana.mylymukk@gmail.com>
2024-11-17 02:31:18 +0200

    changed db

--- BakeryInventory/settings.py
+++ BakeryInventory/settings.py
@@ -93,8 +93,8 @@ DATABASES = {
     }
 }
 
-# database_url = os.environ.get('DATABASE_URL')
-# DATABASES['default']=dj_database_url.parse(database_url)
+database_url = os.environ.get('DATABASE_URL')
+DATABASES['default']=dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
